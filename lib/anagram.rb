7d4133ad998af856ca2dbae0e89cf45904cd7172
Justin Luciano <justinlluciano@gmail.com>
2022-02-01 09:58:30 -0800

    commit tree 564792c2b1fb46fbea63e50c4c5c15b8c78120ab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    word_array = @word.split('')
+    array.select { |new_word| new_word.split('').sort == word_array.sort }
+  end
+end
